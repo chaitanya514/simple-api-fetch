14c8caa63194c5ab504c17ee93db7ea59ad18e97
chaipa <chaitanya.patel@flemingcollege.ca>
2020-07-29 10:31:37 -0400

    simple api fetch and display

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import axios from "axios"
 function App() {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://www.reddit.com/r/reactjs.json`).then(res => {
+      const newPosts = res.data.data.children.map(obj => obj.data);
+      setPosts(newPosts);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {posts.map(post => {
+          return <li key={post.id}>{post.title}{post.score}{post.author}</li>
+
+        })}
+      </ul>
     </div>
   );
 }
